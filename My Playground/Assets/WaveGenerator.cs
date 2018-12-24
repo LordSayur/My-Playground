b05a8cbc1ade60d2bff103cfd79b2853b9eb2e50
OMARV\Omar <mohammad.omar.317@gmail.com>
2018-12-24 09:02:56 +0800

    Add Project Folder

---
+++ My Playground/Assets/WaveGenerator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveGenerator : MonoBehaviour
+{
+    void Update()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+        //Vector3[] normals = mesh.normals;
+        int i = 0;
+        while (i < vertices.Length)
+        {
+            vertices[i] += Vector3.up * Mathf.Sin(Time.time);
+            i++;
+        }
+        mesh.vertices = vertices;
+    }
+}
